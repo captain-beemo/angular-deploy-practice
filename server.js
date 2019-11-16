85a5486730dda4a50d073fc9e8ed806c5af36eb1
jzequn <jzequn@outlook.com>
2019-11-16 23:27:21 +1300

    update server.js

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/dist'));
-
+app.use(express.static(__dirname));
+app.use((req,res,next)=>{
+  response.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
+  next();
+})
 app.all('*', (req, res) => {
   res.status(200).sendFile(__dirname + '/dist/index.html');
 });
